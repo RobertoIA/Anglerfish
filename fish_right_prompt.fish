6725f169905a6564c0e455bc034c333f2612cb29
Roberto Izquierdo <robertoia1987@gmail.com>
2016-02-24 21:41:00 +0100

    Adds time elapsed to right prompt.

---
+++ fish_right_prompt.fish
@@ -0,0 +1,14 @@
+function angler_color_test; set_color -o fee; end
+
+function fish_right_prompt
+	if [ "$CMD_DURATION" -lt 100 ]; and return
+	else if [ "$CMD_DURATION" -lt 1000 ]
+		printf (angler_color_test)$CMD_DURATION'ms '
+	else if [ "$CMD_DURATION" -lt 60000 ]
+		printf (angler_color_test)(math "scale=1;$CMD_DURATION/1000")'s '
+	else if [ "$CMD_DURATION" -lt 3600000 ]
+		printf (angler_color_test)(math "scale=1;$CMD_DURATION/60000")'m '
+	else
+		printf (angler_color_test)(math "scale=1;$CMD_DURATION/3600000")'h '
+	end
+end
