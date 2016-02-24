853b138bdce88be276047813b9ce036bd4694845
Roberto Izquierdo <robertoia1987@gmail.com>
2016-02-24 22:04:05 +0100

    Moves time calculations to separate functions.

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,17 +1,25 @@
 function angler_color_test; set_color -o fee; end
 
-function fish_right_prompt
-	if [ "$CMD_DURATION" -lt 100 ]
-		printf (angler_color_test)' '
+function angler_elapsed
+	if [ "$CMD_DURATION" -lt 100 ]; return
 	else if [ "$CMD_DURATION" -lt 1000 ]
-		printf (angler_color_test)$CMD_DURATION'ms '
+		echo -ns $CMD_DURATION'ms'
 	else if [ "$CMD_DURATION" -lt 60000 ]
-		printf (angler_color_test)(math "scale=1;$CMD_DURATION/1000")'s '
+		echo -ns (math "scale=1;$CMD_DURATION/1000")'s'
 	else if [ "$CMD_DURATION" -lt 3600000 ]
-		printf (angler_color_test)(math "scale=1;$CMD_DURATION/60000")'m '
+		echo -ns (math "scale=1;$CMD_DURATION/60000")'m'
 	else
-		printf (angler_color_test)(math "scale=1;$CMD_DURATION/3600000")'h '
+		echo -ns (math "scale=1;$CMD_DURATION/3600000")'h'
 	end
+end
 
-	printf (date +%H:%M:%S)
+function angler_current
+	echo -ns (date +%H:%M:%S)
+end
+
+function fish_right_prompt
+	set -l elapsed (angler_elapsed)
+	set -l current (angler_current)
+
+	printf (angler_color_test)"$elapsed $current"
 end
