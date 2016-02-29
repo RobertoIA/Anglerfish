fb87fa8874f6e0b0da4ea0600c450b63a27dd930
Roberto Izquierdo <RobertoIA1987@gmail.com>
2016-02-29 13:18:57 +0100

    Adds color definitions for the prompt.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,5 +1,13 @@
+function angler_cl_norm; set_color normal; end
+function angler_cl_esca; set_color -o 268bd2; end
+function angler_cl_off; set_color -o b58900; end
+function angler_cl_on; set_color -o dc322f; end
+function angler_cl_path; set_color 1b85b8; end
+function angler_cl_git; set_color 559e83; end
+
 function angler_dir
 	set -l project (command git rev-parse --show-toplevel 2> /dev/null)
+	angler_cl_path
 
 	if [ -n "$project" ]
 		set -l path (echo -ns "$project" | sed -e 's/.*\///g')
@@ -14,20 +22,23 @@ function angler_dir
 	else
 		echo -ns (prompt_pwd)" "
 	end
+	angler_cl_norm
 end
 
 function angler_branch
+	angler_cl_git
 	echo -ns " "(command git rev-parse --abbrev-ref HEAD 2> /dev/null)
+	angler_cl_norm
 end
 
 function angler_lure
-	echo -ns (set_color blue)"╭"
+	echo -ns (angler_cl_esca)"╭"
 	if [ "$argv[1]" -eq 0 ];
-		echo -ns (set_color yellow)"☼  "
+		echo -ns (angler_cl_off)"☼  "
 	else
-		echo -ns (set_color red)"☼⌁ "
+		echo -ns (angler_cl_on)"☼⌁ "
 	end
-	set_color normal
+	angler_cl_norm
 end
 
 function fish_prompt
